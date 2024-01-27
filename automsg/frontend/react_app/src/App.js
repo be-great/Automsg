335a08a584e85aa9df5b50b4524b18f00cfca991
Ismaila Muhammed <mdmuhab@gmail.com>
2024-01-27 20:22:01 +0100

    added bootstrap and react-bootstrap

--- automsg/frontend/react_app/src/App.js
+++ automsg/frontend/react_app/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Auto Message to WhatsApp and Discord
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Submit</Button>
       </header>
     </div>
   );
