ee0112f776f2248971b9ee8f670af68490fe4196
Forever Inc dev <redodevs@gmail.com>
2024-01-31 13:29:23 +0300

    Update views.py
    Custom Views to allow cookie settings

--- automsg/backend/project_automsg/accounts/views.py
+++ automsg/backend/project_automsg/accounts/views.py
@@ -7,17 +7,103 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from accounts.permisions import IsSuperuserOrReadOnly
-from accounts.serializers.accaountsSerializers import RegisterSerializer, ChangePasswordSerializer, \
-    UpdateUserSerializer, UserSerializer
+from accounts.serializers.accaountsSerializers import (
+    RegisterSerializer,
+    ChangePasswordSerializer,
+    UpdateUserSerializer,
+    UserSerializer,
+)
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,TokenVerifyView
+from django.conf import settings
 
 User = get_user_model()
+
+
+# Custom ObtainPairView
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            access_token = response.data.get("access")
+            refresh_token = response.data.get("refresh")
+
+            response.set_cookie(
+                settings.ACCESS_COOKIE_NAME,
+                access_token,
+                httponly=True,
+                max_age=settings.ACCESS_COOKIE_MAX_AGE,
+                secure=settings.AUTH_COOKIE_SECURE,
+                path="/",
+                samesite="Lax",
+            )
+
+            response.set_cookie(
+                settings.REFRESH_COOKIE_NAME,
+                refresh_token,
+                httponly=True,
+                max_age=settings.REFRESH_COOKIE_MAX_AGE,
+                secure=settings.AUTH_COOKIE_SECURE,
+                path="/",
+                samesite="Lax",
+            )
+        return response
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get(settings.REFRESH_COOKIE_NAME)
+
+        if refresh_token:
+            request.data["refresh"] = refresh_token
+
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            access_token = response.data.get("access")
+            refresh_token = response.data.get("refresh")
+
+            response.set_cookie(
+                settings.ACCESS_COOKIE_NAME,
+                access_token,
+                httponly=True,
+                max_age=settings.ACCESS_COOKIE_MAX_AGE,
+                secure=settings.AUTH_COOKIE_SECURE,
+                path="/",
+                samesite="Lax",
+            )
+            # refresh token must be reset if the blacklist option is enabled in the settings file.
+        return response
+
+
+
+# for verification purposes if required
+class CustomTokenVerifyView(TokenVerifyView):
+    def post(self, request, *args, **kwargs):
+        access_token = request.COOKIES.get(settings.AUTH_COOKIE_NAME)
+
+        if access_token:
+            request.data["token"] = access_token
+
+        return super().post(request, *args, **kwargs)
+
+
+
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['first_name', 'last_name']
-    permission_classes = (IsAuthenticated, IsSuperuserOrReadOnly,)
+    filterset_fields = ["first_name", "last_name"]
+    permission_classes = (
+        IsAuthenticated,
+        IsSuperuserOrReadOnly,
+    )
+
 
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
