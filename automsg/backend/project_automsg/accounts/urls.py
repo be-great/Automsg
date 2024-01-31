7db9ab74f4ae6e4b17cd6d65fcede315468c2a73
Forever Inc dev <redodevs@gmail.com>
2024-01-31 13:28:52 +0300

    Update urls.py - Custom Views to inforce cookies

--- automsg/backend/project_automsg/accounts/urls.py
+++ automsg/backend/project_automsg/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from accounts.views import RegisterView, LogoutView, ChangePasswordView, UpdateProfileView, UserViewSet
+
+from accounts.views import RegisterView, LogoutView, ChangePasswordView, UpdateProfileView, UserViewSet,CustomTokenObtainPairView,CustomTokenRefreshView
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
@@ -12,8 +12,8 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("change-password/<int:pk>", ChangePasswordView.as_view(), name="change-password"),
     path("update-profile/<int:pk>", UpdateProfileView.as_view(), name="update-profile"),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
